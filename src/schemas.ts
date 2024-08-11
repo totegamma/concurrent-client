d1bc993927e79e3ea86af7c7a3dfaa2b3c2223da
totegamma <github@gammalab.net>
2024-08-12 00:14:59 +0900

    add new schema

--- src/schemas.ts
+++ src/schemas.ts
@@ -2,13 +2,15 @@ export const Schemas = {
     markdownMessage:     'https://schema.concrnt.world/m/markdown.json',
     replyMessage:        'https://schema.concrnt.world/m/reply.json',
     rerouteMessage:      'https://schema.concrnt.world/m/reroute.json',
+    plaintextMessage:    'https://schema.concrnt.world/m/plaintext.json',
+    mediaMessage:        'https://schema.concrnt.world/m/media.json',
 
     likeAssociation:     'https://schema.concrnt.world/a/like.json',
     mentionAssociation:  'https://schema.concrnt.world/a/mention.json',
     replyAssociation:    'https://schema.concrnt.world/a/reply.json',
     rerouteAssociation:  'https://schema.concrnt.world/a/reroute.json',
     reactionAssociation: 'https://schema.concrnt.world/a/reaction.json',
-    upgradeAssociation: 'https://schema.concrnt.world/a/upgrade.json',
+    upgradeAssociation:  'https://schema.concrnt.world/a/upgrade.json',
 
     profile:             'https://schema.concrnt.world/p/main.json',
 
